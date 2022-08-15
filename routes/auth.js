33aba8dac18e07a1e20f1ddfa1d0e885e4fee4bb
Corey Damocles <97559559+ubemacapuno@users.noreply.github.com>
2022-08-14 17:00:27 -0700

    Additional auth functionality

--- routes/auth.js
+++ routes/auth.js
@@ -20,8 +20,10 @@ router.get(
 // @route   /auth/logout
 //!Change: Passport 0.6 requires logout to be async
 router.get('/logout', (req, res) => {
-  req.logout()
-  res.redirect('/')
+  req.logout(function(err) {
+    if (err) {return next(err); }
+    res.redirect('/')
+  })
 })
 
 module.exports = router
